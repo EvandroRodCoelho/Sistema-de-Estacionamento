771d2fd6950987caf6cbde1f46a01766a7836927
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2022-07-08 14:44:04 -0300

    Modificando projeto

--- src/dist/index.js
+++ src/dist/index.js
@@ -1,63 +1,61 @@
-(function () {
-    var _a;
-    const $ = (query) => document.querySelector(query);
-    function calculateTime(mil) {
-        const min = Math.floor(mil / 60000);
-        const seg = Math.floor((mil % 60000) / 1000);
-        return `${min} minutos  e ${seg} segundos`;
+var _a;
+const $ = (query) => document.querySelector(query);
+function calculateTime(mil) {
+    const min = Math.floor(mil / 60000);
+    const seg = Math.floor((mil % 60000) / 1000);
+    return `${min} minutos  e ${seg} segundos`;
+}
+function parking() {
+    function read() {
+        return localStorage.parking ? JSON.parse(localStorage.parking) : [];
     }
-    function parking() {
-        function read() {
-            return localStorage.parking ? JSON.parse(localStorage.parking) : [];
-        }
-        function save(vehicles) {
-            localStorage.setItem("parking", JSON.stringify(vehicles));
-        }
-        function add(vehicle, toSaves) {
-            var _a, _b;
-            const row = document.createElement("tr");
-            row.innerHTML = `
-                <td>${vehicle.name}</td>
+    function save(vehicles) {
+        localStorage.setItem("parking", JSON.stringify(vehicles));
+    }
+    function add(vehicle, toSaves) {
+        var _a, _b;
+        const row = document.createElement("tr");
+        row.innerHTML = `
+                <td class="text-wrap ">${vehicle.name}</td>
                 <td>${vehicle.licensePlate}</td>
                 <td>${vehicle.time}</td>
-                <td>
-                    <button id="delete" data-licensePlate="${vehicle.licensePlate}"
+                <td class="p-2">
+                    <button id="delete" data-licenseplate="${vehicle.licensePlate}"
                     class="btn btn-danger">X</button>
                 </td>
             `;
-            (_a = row.querySelector("#delete")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
-                remove(this.dataset.licensePlate);
-            });
-            (_b = $("#parking")) === null || _b === void 0 ? void 0 : _b.appendChild(row);
-            if (toSaves)
-                save([...read(), vehicle]);
-        }
-        function remove(licensePlate) {
-            const { time, name } = read().find(vehicle => vehicle.licensePlate !== licensePlate);
-            const totalTime = calculateTime(new Date().getTime() - new Date(time).getTime());
-            if (!confirm(`O veículo ${name} Permaneceu por ${totalTime}, deseja encerrar?`))
-                return;
-            save(read().filter(vehicle => vehicle.licensePlate !== licensePlate));
-            render();
-        }
-        function render() {
-            $("#parking").innerHTML = "";
-            const parking = read();
-            if (parking.length) {
-                parking.forEach(vehicle => add(vehicle));
-            }
-        }
-        return { read, remove, add, save, render };
+        (_a = row.querySelector("#delete")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+            remove(this.dataset.licenseplate);
+        });
+        (_b = $("#parking")) === null || _b === void 0 ? void 0 : _b.appendChild(row);
+        if (toSaves)
+            save([...read(), vehicle]);
     }
-    parking().render();
-    (_a = $("#register")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
-        var _a, _b;
-        const name = (_a = $("#name")) === null || _a === void 0 ? void 0 : _a.value;
-        const licensePlate = (_b = $("#licensePlate")) === null || _b === void 0 ? void 0 : _b.value;
-        if (!name || !licensePlate) {
-            alert("DIgite todos os items");
+    function remove(licensePlate) {
+        const { time, name } = read().find(vehicle => vehicle.licensePlate === licensePlate);
+        const totalTime = calculateTime(new Date().getTime() - new Date(time).getTime());
+        if (!confirm(`O veículo ${name} Permaneceu por ${totalTime}, deseja encerrar?`))
             return;
+        save(read().filter(vehicle => vehicle.licensePlate !== licensePlate && vehicle.time !== time));
+        render();
+    }
+    function render() {
+        $("#parking").innerHTML = "";
+        const parking = read();
+        if (parking.length) {
+            parking.forEach(vehicle => add(vehicle));
         }
-        parking().add({ name, licensePlate, time: new Date().toISOString() }, true);
-    });
-})();
+    }
+    return { read, remove, add, save, render };
+}
+parking().render();
+(_a = $("#register")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
+    var _a, _b;
+    const name = (_a = $("#name")) === null || _a === void 0 ? void 0 : _a.value;
+    const licensePlate = (_b = $("#licensePlate")) === null || _b === void 0 ? void 0 : _b.value;
+    if (!name || !licensePlate) {
+        alert("DIgite todos os items");
+        return;
+    }
+    parking().add({ name, licensePlate, time: new Date().toISOString() }, true);
+});
--- src/index.ts
+++ src/index.ts
@@ -5,71 +5,71 @@ interface Vehicle {
 }
 
 
-(function () {
-    const $ = (query: string): HTMLInputElement | null => document.querySelector(query)
-    function calculateTime(mil: number) {
-        const min = Math.floor(mil / 60000)
-        const seg = Math.floor((mil % 60000) / 1000)
-        return `${min} minutos  e ${seg} segundos`
-    }
 
-    function parking() {
-        function read(): Vehicle[] {
-            return localStorage.parking ? JSON.parse(localStorage.parking) : []
-        }
-        function save(vehicles: Vehicle[]) {
-            localStorage.setItem("parking", JSON.stringify(vehicles))
-        }
-        function add(vehicle: Vehicle, toSaves?: boolean) {
-            const row = document.createElement("tr")
-            row.innerHTML = `
-                <td>${vehicle.name}</td>
+const $ = (query: string): HTMLInputElement | null => document.querySelector(query)
+function calculateTime(mil: number) {
+    const min = Math.floor(mil / 60000)
+    const seg = Math.floor((mil % 60000) / 1000)
+    return `${min} minutos  e ${seg} segundos`
+}
+
+function parking() {
+    function read(): Vehicle[] {
+        return localStorage.parking ? JSON.parse(localStorage.parking) : []
+    }
+    function save(vehicles: Vehicle[]) {
+        localStorage.setItem("parking", JSON.stringify(vehicles))
+    }
+    function add(vehicle: Vehicle, toSaves?: boolean) {
+        const row = document.createElement("tr")
+        row.innerHTML = `
+                <td class="text-wrap ">${vehicle.name}</td>
                 <td>${vehicle.licensePlate}</td>
                 <td>${vehicle.time}</td>
-                <td>
-                    <button id="delete" data-licensePlate="${vehicle.licensePlate}"
+                <td class="p-2">
+                    <button id="delete" data-licenseplate="${vehicle.licensePlate}"
                     class="btn btn-danger">X</button>
                 </td>
             `
-            row.querySelector("#delete")?.addEventListener("click", function () {
-                remove(this.dataset.licensePlate)
-            })
-            $("#parking")?.appendChild(row)
-            if (toSaves) save([...read(), vehicle])
-        }
-        function remove(licensePlate: string) {
-            const { time, name } = read().find(vehicle => vehicle.licensePlate !== licensePlate)
-
-            const totalTime = calculateTime(new Date().getTime() - new Date(time).getTime())
-            if (
-                !confirm(`O veículo ${name} Permaneceu por ${totalTime}, deseja encerrar?`)
-            ) return
-
-            save(read().filter(vehicle => vehicle.licensePlate !== licensePlate))
-            render()
-        }
-
-        function render() {
-            $("#parking")!.innerHTML = ""
-            const parking = read()
-            if (parking.length) {
-                parking.forEach(vehicle => add(vehicle))
-            }
-        }
-
-        return { read, remove, add, save, render }
+        row.querySelector("#delete")?.addEventListener("click", function () {
+            remove(this.dataset.licenseplate)
+        })
+        $("#parking")?.appendChild(row)
+        if (toSaves) save([...read(), vehicle])
     }
-    parking().render()
+    function remove(licensePlate: string) {
 
-    $("#register")?.addEventListener("click", () => {
-        const name = $("#name")?.value
-        const licensePlate = $("#licensePlate")?.value
+        const { time, name } = read().find(vehicle => vehicle.licensePlate === licensePlate);
 
 
-        if (!name || !licensePlate) {
-            alert("DIgite todos os items")
-            return
+        const totalTime = calculateTime(new Date().getTime() - new Date(time).getTime())
+        if (
+            !confirm(`O veículo ${name} Permaneceu por ${totalTime}, deseja encerrar?`)
+        ) return
+
+        save(read().filter(vehicle => vehicle.licensePlate !== licensePlate && vehicle.time !== time))
+        render()
+    }
+
+    function render() {
+        $("#parking")!.innerHTML = ""
+        const parking = read()
+        if (parking.length) {
+            parking.forEach(vehicle => add(vehicle))
         }
-        parking().add({ name, licensePlate, time: new Date().toISOString() }, true)
-    })
-})()
+    }
+
+    return { read, remove, add, save, render }
+}
+parking().render()
+
+$("#register")?.addEventListener("click", () => {
+    const name = $("#name")?.value
+    const licensePlate = $("#licensePlate")?.value
+
+    if (!name || !licensePlate) {
+        alert("DIgite todos os items")
+        return
+    }
+    parking().add({ name, licensePlate, time: new Date().toISOString() }, true)
+})
